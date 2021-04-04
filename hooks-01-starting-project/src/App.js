4166e5dc865675a3e7fae04297df164faa725d81
alisdemir33 <alisdemir33@gmail.com>
2021-04-04 10:42:16 +0300

    ilk

---
+++ hooks-01-starting-project/src/App.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import {AuthContext} from './context/Auth-Context'
+
+import Ingredients from './components/Ingredients/Ingredients';
+import Auth from './components/Auth'
+
+import { render } from 'react-dom';
+
+const App = props => {
+  const authContext = useContext(AuthContext);
+
+  console.log('custom hooks')
+  
+  let content =<Auth></Auth>
+  if(authContext.isAuth)
+  {
+      content =<Ingredients></Ingredients>
+  }  
+
+  return (content);
+
+};
+
+export default App;
